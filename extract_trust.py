9d6c5e9d150cc37c80523a28f14a16864c78d26d
H1MSK <ksda47832338@outlook.com>
2023-12-17 15:13:51 +0800

    export trust to csv

--- extract_trust.py
+++ extract_trust.py
@@ -3,7 +3,7 @@ import json
 
 
 def main():
-    with open("operators.json", "r", encoding='utf8') as f:
+    with open("operators.json", "r", encoding='utf-8') as f:
         data = json.load(f)
 
     counts = [0 for _ in range(201)]
@@ -11,8 +11,8 @@ def main():
     for i in data:
         counts[i['trust']] += 1
 
-    for i in range(201):
-        print(f"{i}: {counts[i]}")
+    with open("trust.csv", "w") as f:
+        f.write("\n".join(f"{i}: {counts[i]}" for i in range(201)))
 
 if __name__ == '__main__':
     main()
